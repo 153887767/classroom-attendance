f9e4e9b0d60ea4f95b2ba35857440e2a744985d1
caizhaojie <153887767@qq.com>
2023-04-20 16:45:36 +0800

    chore: vant 组件

--- client-wx/miniprogram/app.ts
+++ client-wx/miniprogram/app.ts
@@ -9,10 +9,10 @@ App<IAppOption>({
 
     // 登录
     wx.login({
-      success: res => {
-        console.log(res.code)
+      success: (res) => {
+        console.log('res.code', res.code)
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      },
+      }
     })
-  },
+  }
 })
