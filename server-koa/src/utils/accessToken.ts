983e1b94fc9c95b901f734148004f5efec2920e5
caizhaojie <153887767@qq.com>
2023-04-21 20:46:26 +0800

    chore: 添加注释

--- server-koa/src/utils/accessToken.ts
+++ server-koa/src/utils/accessToken.ts
@@ -4,6 +4,7 @@ import { AK, SK } from '../conf/secretKeys'
 const URL = `https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=${AK}&client_secret=${SK}`
 
 /**
+ * 百度人脸识别服务
  * 使用 AK，SK 生成鉴权签名（Access Token）
  * @return string 鉴权签名信息（Access Token）
  */
