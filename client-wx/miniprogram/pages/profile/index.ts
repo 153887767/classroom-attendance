ad7a698ab54f4fab8b398a46e46eec29d479ec3a
caizhaojie <153887767@qq.com>
2023-04-21 16:51:33 +0800

    feat: 获取学生信息 & 修改学号与姓名

--- client-wx/miniprogram/pages/profile/index.ts
+++ client-wx/miniprogram/pages/profile/index.ts
@@ -1,66 +1,107 @@
-// pages/profile/index.ts
+import { request, HttpMethod } from '../../utils/request'
+
 Page({
+  data: {
+    userName: '',
+    studentNumber: '',
+    avatar: '',
+    faceImg: '',
+    studentNumberModified: true,
+    userNameModified: true
+  },
+
+  handleChooseAvatar(e: WechatMiniprogram.TouchEvent) {
+    const { avatarUrl } = e.detail
+    console.log(avatarUrl)
+    this.setData({
+      avatarUrl
+    })
+  },
 
   /**
-   * 页面的初始数据
+   * 修改学号
    */
-  data: {
+  changeStudentNumber() {
+    const that = this
+    wx.showModal({
+      title: '修改学号',
+      content: `学号只能修改一次，请仔细核对：${this.data.studentNumber}`,
+      async success(res) {
+        if (res.confirm) {
+          const result: any = await request(
+            '/api/student/studentNumber',
+            HttpMethod.POST,
+            {
+              studentNumber: that.data.studentNumber
+            }
+          )
+          if (result.data?.errno === 0) {
+            wx.showToast({
+              title: '修改学号成功',
+              icon: 'success',
+              duration: 1000
+            })
+            that.setData({
+              studentNumberModified: true
+            })
+          } else {
+            wx.showToast({
+              title: '修改学号失败',
+              icon: 'error' as any,
+              duration: 1000
+            })
+          }
+        }
+      }
+    })
+  },
 
+  /**
+   * 修改姓名
+   */
+  changeUserName() {
+    const that = this
+    wx.showModal({
+      title: '修改姓名',
+      content: `姓名只能修改一次，请仔细核对：${this.data.userName}`,
+      async success(res) {
+        if (res.confirm) {
+          const result: any = await request(
+            '/api/student/userName',
+            HttpMethod.POST,
+            {
+              userName: that.data.userName
+            }
+          )
+          if (result.data?.errno === 0) {
+            wx.showToast({
+              title: '修改姓名成功',
+              icon: 'success',
+              duration: 1000
+            })
+            that.setData({
+              userNameModified: true
+            })
+          } else {
+            wx.showToast({
+              title: '修改姓名失败',
+              icon: 'error' as any,
+              duration: 1000
+            })
+          }
+        }
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+    request('/api/student/getInfo', HttpMethod.GET).then((res: any) => {
+      if (res.data?.errno === 0) {
+        this.setData(res.data.data)
+      }
+    })
   }
 })
