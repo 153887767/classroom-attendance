ce7da4c80e318eb8ece672032baf63536329924b
caizhaojie <153887767@qq.com>
2023-04-23 15:49:03 +0800

    feat: 小程序扫码选课、课程列表展示

--- client-wx/miniprogram/pages/lessons/index.ts
+++ client-wx/miniprogram/pages/lessons/index.ts
@@ -1,11 +1,30 @@
-Page({
-  data: {},
+import { request, HttpMethod } from '../../utils/request'
 
+Page({
+  data: {
+    count: 0, // 课程总数
+    lessonList: [] // 课程列表
+  },
+
+  /**
+   * 扫描课程二维码加入课程
+   */
   handleScanCode() {
+    const that = this
     wx.scanCode({
       scanType: ['qrCode'],
-      success(res) {
-        console.log(res.result)
+      async success(res) {
+        const result: any = await request(
+          '/api/student/selectLesson',
+          HttpMethod.POST,
+          {
+            lessonId: res.result
+          }
+        )
+        if (result.data?.errno === 0) {
+          const { count, lessonList } = result.data.data
+          that.setData({ count, lessonList })
+        }
       }
     })
   },
@@ -13,5 +32,12 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad() {}
+  onLoad() {
+    request('/api/student/lessonsList', HttpMethod.GET).then((res: any) => {
+      if (res.data?.errno === 0) {
+        const { count, lessonList } = res.data.data
+        this.setData({ count, lessonList })
+      }
+    })
+  }
 })
