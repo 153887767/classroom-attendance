0dffe7bcf66b0fc1ef558eb21d0af027a2eba40f
caizhaojie <153887767@qq.com>
2023-04-24 13:45:04 +0800

    feat: 获取当前需要考勤的课程

--- client-wx/miniprogram/pages/attendance/index.ts
+++ client-wx/miniprogram/pages/attendance/index.ts
@@ -1,66 +1,23 @@
-// pages/attendance/index.ts
+import { request, HttpMethod } from '../../utils/request'
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    currentLesson: null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  async onLoad() {
+    const res: any = await request(
+      '/api/attendance/currentLesson',
+      HttpMethod.GET
+    )
+    if (res.data?.errno === 0 && res.data.data.currentLesson) {
+      console.log(res)
+      this.setData({
+        currentLesson: res.data.data.currentLesson
+      })
+    }
   }
 })
